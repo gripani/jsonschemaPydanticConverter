37f1b083992170b40709b677db87a87ee7523930
gripani <g.ripani93@gmail.com>
2024-01-21 18:57:55 +0100

    added module_name cli argument and logging

--- jsonschema_converter/parse.py
+++ jsonschema_converter/parse.py
@@ -1,24 +1,34 @@
 from json import load
 from os import mkdir
 from os.path import exists, join
+from sys import stderr
 
+from loguru import logger
+
+from jsonschema_converter.const import FORMAT
 from jsonschema_converter.schema_parser import SchemaParser
 
+logger.remove()
+logger.add(stderr, level="INFO", format=FORMAT)
 
-def parse_json(file_name: str):
+
+def parse_json_schema(file_name: str, module_name: str | None = None):
+
+    if module_name is None:
+        module_name = "schemas"
 
     with open(file_name, "r") as f:
         schema = load(f)
 
-    if not exists("schemas"):
-        mkdir("schemas")
-
-    with open(join("schemas", "__init__.py"), "w") as f0:
-        f0.write("")
+    if not exists(module_name):
+        mkdir(module_name)
+        with open(join(module_name, "__init__.py"), "w") as f0:
+            f0.write("")
 
     try:
         schema_parser = SchemaParser(schema, name=file_name.split(".json")[0])
+        schema_parser.parse(module_name)
     except ValueError as err:
-        print(err)
+        detail=str(err)
+        logger.error(f"parse_json - ValueError:\n{detail=}")
         exit()
-    schema_parser.parse()
